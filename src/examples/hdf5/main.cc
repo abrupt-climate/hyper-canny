b80a469af88f408cdb34440e8b1ddc109c99ef2a
Johannes Hidding <johannes.hidding@gmail.com>
2017-04-14 13:50:30 +0200

    extend hdf5 example

--- src/examples/hdf5/main.cc
+++ src/examples/hdf5/main.cc
@@ -18,6 +18,26 @@ void convert_hdf5_to_png()
     if (type_class != H5T_FLOAT)
         throw Exception("Expected a field with floating-point values.");
 
+    H5::FloatType type = dataset.getFloatType();
+
+    std::string endianness;
+    switch (type.getOrder())
+    {
+        case H5T_ORDER_LE: endianness = "little endian"; break;
+        case H5T_ORDER_BE: endianness = "big endian"; break;
+        case H5T_ORDER_VAX: endianness = "vax"; break;
+        default: endianness = "unknown endianness"; break;
+    }
+
+    console.msg(type.getPrecision(), " bit, ", endianness, ", floating point data.");
+
+    H5::DataSpace dataspace = dataset.getSpace();
+    if (not dataspace.isSimple())
+        throw Exception("data space is not simple.");
+    int dim = dataspace.getSimpleExtentNdims();
+    std::vector<hsize_t> shape(dim);
+    dataspace.getSimpleExtentDims(shape.data(), nullptr);
+    console.msg("simple data with shape (", string_join(shape, ", "), ")");
     console.finish("Done");
 }
 
