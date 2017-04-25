1f80f6254f3a0caa06246203a96c1d0ac0280979
Johan Hidding <johannes.hidding@gmail.com>
2017-04-25 20:35:53 +0200

    add header notices

--- test/numeric/filters.cc
+++ test/numeric/filters.cc
@@ -1,3 +1,17 @@
+/* Copyright 2017 Netherlands eScience Center
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
 #include <gtest/gtest.h>
 #include <iostream>
 #include "base.hh"
@@ -76,4 +90,3 @@ TEST (Filters, Sobel)
     assert_array_equal(filter::sobel(a1, 0), b1);
     assert_array_equal(filter::sobel(a1, 1), b2);
 }
-
